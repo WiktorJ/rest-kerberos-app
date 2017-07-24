af3bc55c76e86686061c4815ff0f69ca8967a3ff
wjurasz <wiktor.jan.jurasz@cern.ch>
2017-07-24 09:47:57 +0200

    further experimetns

---
+++ src/main/java/test/GlobalSecurityConfig.java
@@ -0,0 +1,7 @@
+package test;
+
+/**
+ * Created by wjurasz on 21.07.17.
+ */
+public class GlobalSecurityConfig {
+}
---
+++ src/main/java/test/TestClient.java
@@ -0,0 +1,7 @@
+package test;
+
+/**
+ * Created by wjurasz on 20.07.17.
+ */
+public class TestClient {
+}
